eaef072260fae74afa2ffc909e9ce8082d331e99
tarun chaudhary <tarun0448@gmail.com>
2021-09-12 23:06:24 +0530

    about page edited

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ export default function About() {
             <div className="container my-3 text-center" style={myStyle}>
                 <h5>About Keep Notes App</h5>
                 <div className="text-justify">
-                    <h6>What is a Keep Notes List?</h6>
+                    <h6>What is a Keep Notes App?</h6>
                     <p> What is a Keep Notes List? The definition is a simple one. It’s a list of tasks you need to complete, or things that you want to do.</p>
 
                     <p> Most typically, they’re organised in order of priority. Traditionally, they’re written on a piece of paper or post it notes and acts as a memory aid. As technology has evolved we have been able to create a Keep Notes lists with excel spreadsheets, word documents, email lists, Keep Notes list apps, microsoft to do and google to do list to name a few. You can use a to do list in your home and personal life, or in the workplace.</p>
